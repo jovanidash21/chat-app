689fb7431dfdfdc588112ed052e2c4c2e971c548
jovanidash21 <jwardash21@gmail.com>
2017-04-05 21:48:23 +0800

    Add Users Data Schema

---
+++ models/users-data-schema.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose');
+var Promise = require('bluebird');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+var timestamps = require('mongoose-timestamp');
+
+mongoose.Promise = Promise;
+
+var usersDataSchema = new Schema
+(
+    {
+        firstName: {type:String, default: ''},
+        lastName: {type:String, default: ''},
+        email: {type:String, default: ''},
+        socialLinks: [{
+            website: {type: String, default: ''},
+            facebook: {type: String, default: ''},
+            twitter: {type: String, default: ''},
+            instagram: {type: String, default: ''},
+            googleplus: {type: String, default: ''},
+            linkedin: {type: String, default: ''},
+            youtube: {type: String, default: ''},
+            github: {type: String, default: ''},
+            codepen: {type: String, default: ''}
+        }]
+    },
+    {
+        collection: 'usersData'
+    }
+);
+
+usersDataSchema.plugin(passportLocalMongoose);
+usersDataSchema.plugin(timestamps);
+
+module.exports = mongoose.model('usersData', usersDataSchema);
