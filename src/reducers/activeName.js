db87e1e9419ef0843a699f9d1ab5987c6f1f1c89
jovanidash21 <jwardash21@gmail.com>
2017-04-13 21:20:23 +0800

    Delete Default React Redux Layout

--- src/reducers/activeName.js
+++
@@ -1,8 +0,0 @@
-export default function (state = null, action) {
-    switch (action.type) {
-        case 'NAME_SELECTED':
-            return action.payload;
-            break;
-    }
-    return state;
-}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,7 @@
 import { combineReducers } from 'redux';
-import namesReducer from './names';
-import activeNameReducer from './activeName';
 
 const reducers = combineReducers({
-    names: namesReducer,
-    activeName: activeNameReducer
+
 });
 
 export default reducers;
