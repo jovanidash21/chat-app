d2723265a98a3f519cf81aaa2ef2488260c1c3ce
jovanidash21 <jwardash21@gmail.com>
2017-04-19 21:59:36 +0800

    Add Passport Config

--- app.js
+++ app.js
@@ -4,11 +4,40 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var flash = require('connect-flash');
+var mongoose = require('mongoose');
+var Promise = require('bluebird');
+
+var options = {
+  server: {
+    socketOptions: {
+      keepAlive: 300000, connectTimeoutMS: 30000
+    }
+  },
+  replset: {
+    socketOptions: {
+      keepAlive: 300000, connectTimeoutMS : 30000
+    }
+  }
+};
 
 var index = require('./routes/index');
 var api = require('./routes/api');
 
+var usersData = require('./models/users-data-schema');
+
 var app = express();
+
+passport.use(new LocalStrategy(usersData.authenticate()));
+passport.serializeUser(usersData.serializeUser());
+passport.deserializeUser(usersData.deserializeUser());
+
+// mongoose configuration
+mongoose.connect(process.env.MONGODB_URI, options);
+mongoose.Promise = Promise;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +50,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  saveUninitialized: true,
+  resave: false
+}));
+
+// passport configuration
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
 
 app.use('/', index);
 app.use('/api', api);
