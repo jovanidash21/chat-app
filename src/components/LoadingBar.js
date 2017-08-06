f91375f7463537581e944f2d0878fb86755d0ae2
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-08-06 22:52:53 +0800

    Add Loading Bar Component And Reducer

---
+++ src/components/LoadingBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import LoadingBar from 'react-redux-loading-bar';
+
+const LoadingBarComponent = () => (
+    <LoadingBar />
+)
+
+export default LoadingBarComponent;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import { loadingBarReducer } from 'react-redux-loading-bar';
 
 const reducers = combineReducers({
-
+    loadingBar: loadingBarReducer,
 });
 
 export default reducers;
