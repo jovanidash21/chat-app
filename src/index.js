f5d77d58a46dce54b80bd86a7c9a693f4c384f79
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-08-10 22:24:06 +0800

    Update Import Syntax

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from "react-redux";
+import { Provider } from 'react-redux';
 import App from './components/App';
-import store from "./store/index";
+import store from './store/index';
 
 render(
     <Provider store={store}>
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
-import { createStore, applyMiddleware} from "redux";
-import { createLogger } from "redux-logger";
-import thunk from "redux-thunk";
-import promise from "redux-promise-middleware";
-import reducers from "../reducers/index";
+import { createStore, applyMiddleware} from 'redux';
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
+import promise from 'redux-promise-middleware';
+import reducers from '../reducers/index';
 
 const store = createStore(
     reducers,
