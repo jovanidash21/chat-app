7d1134ed446ffc065938c8ba2a527c747aea84ec
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-08-24 19:51:57 +0800

    Convert Tab Spaces To 2

--- models/users-data-schema.js
+++ models/users-data-schema.js
@@ -8,25 +8,25 @@ mongoose.Promise = Promise;
 
 var usersDataSchema = new Schema
 (
-    {
-        firstName: {type:String, default: ''},
-        lastName: {type:String, default: ''},
-        email: {type:String, default: ''},
-        socialLinks: [{
-            website: {type: String, default: ''},
-            facebook: {type: String, default: ''},
-            twitter: {type: String, default: ''},
-            instagram: {type: String, default: ''},
-            googleplus: {type: String, default: ''},
-            linkedin: {type: String, default: ''},
-            youtube: {type: String, default: ''},
-            github: {type: String, default: ''},
-            codepen: {type: String, default: ''}
-        }]
-    },
-    {
-        collection: 'usersData'
-    }
+  {
+    firstName: {type:String, default: ''},
+    lastName: {type:String, default: ''},
+    email: {type:String, default: ''},
+    socialLinks: [{
+      website: {type: String, default: ''},
+      facebook: {type: String, default: ''},
+      twitter: {type: String, default: ''},
+      instagram: {type: String, default: ''},
+      googleplus: {type: String, default: ''},
+      linkedin: {type: String, default: ''},
+      youtube: {type: String, default: ''},
+      github: {type: String, default: ''},
+      codepen: {type: String, default: ''}
+    }]
+  },
+  {
+    collection: 'usersData'
+  }
 );
 
 usersDataSchema.plugin(passportLocalMongoose);
