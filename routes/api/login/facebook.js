1e4a2fdd52bf5f91fd41999143b87d4e067a1e6c
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-08-28 22:32:54 +0800

    Update Passport Facebook Login API

--- routes/api/login/facebook.js
+++ routes/api/login/facebook.js
@@ -2,16 +2,45 @@ var express = require('express');
 var router = express.Router({mergeParams: true});
 var passport = require('passport');
 var Strategy = require('passport-facebook').Strategy;
+var usersData = require('../../../models/users-data-schema');
 
 passport.use(new Strategy({
   clientID: process.env.FACEBOOK_CLIENT_ID,
   clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-  callbackURL: '/api/login/facebook/callback'
-}, function(accessToken, refreshToken, profile, done) {
-  return done(null, profile);
+  callbackURL: '/api/login/facebook/callback',
+  profileFields: ['id', 'name', 'emails']
+}, function(req, accessToken, refreshToken, profile, done) {
+  usersData.findOne({profileID: profile.id}, function(err, user) {
+    if (!err) {
+      if (user === null) {
+        var newUser = new usersData({
+          profileID: profile.id,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          email: (profile.emails[0].value || '')
+        });
+        newUser.save(function(err) {
+          if (!err) {
+            return done(null, newUser);
+          } 
+          else {
+            return done(err);
+          }
+        });
+      }
+      else {
+        return done(null, user);
+      }
+    }
+    else {
+      return done(err);
+    }
+  });
 }));
 
-router.get('/', passport.authenticate('facebook', { scope : 'email' }));
+router.get('/', passport.authenticate('facebook', {
+  scope : ['email'] 
+}));
 
 router.get('/callback', passport.authenticate('facebook', { 
   failureRedirect: '/' 
