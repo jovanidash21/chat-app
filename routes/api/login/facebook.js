3bf25831c8c456f7a500abde2116763566632d34
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-08-30 22:34:42 +0800

    Update Passport Facebook Login API

--- routes/api/login/facebook.js
+++ routes/api/login/facebook.js
@@ -8,36 +8,47 @@ passport.use(new Strategy({
   clientID: process.env.FACEBOOK_CLIENT_ID,
   clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
   callbackURL: '/api/login/facebook/callback',
-  profileFields: ['id', 'displayName', 'emails', 'photos']
-}, function(req, accessToken, refreshToken, profile, done) {
+  profileFields: ['id', 'displayName', 'emails', 'picture.type(large)']
+}, function(accessToken, refreshToken, profile, done) {
   var username = 'facebook/' + profile.id;
   var name = profile.displayName;
   var email;
   var profilePicture;
 
-  if (profile.emails[0].value) {
+  if (profile.emails !== undefined) {
     email = profile.emails[0].value;
   }
   else {
     email = '';
   }
-
-  if (profile.photos[0].value) {
+  if (profile.photos !== undefined) {
     profilePicture = profile.photos[0].value;
   }
   else {
     profilePicture = '';
   }
+  
+  var userData = {
+    username: username,
+    name: name,
+    email: email,
+    profilePicture: profilePicture
+  }
 
   usersData.findOne({username: username}, function(err, user) {
     if (!err) {
-      if (user === null) {
-        var newUser = new usersData({
-          username: username,
-          name: name,
-          email: email,
-          profilePicture: profilePicture
+      if (user !== null) {
+        user.update(userData, function(err) {
+          if (!err) {
+            return done(null, user);
+          }
+          else {
+            return done(err);
+          }
         });
+      }
+      else {
+        var newUser = new usersData(userData);
 
         newUser.save(function(err) {
           if (!err) {
@@ -47,9 +58,6 @@ passport.use(new Strategy({
             return done(err);
           }
         });
-      }
-      else {
-        return done(null, user);
       }
     }
     else {
