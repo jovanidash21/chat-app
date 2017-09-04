5405b69ecd7cb5fc2ebf9df77fb2c5e16edb688f
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-09-04 19:56:02 +0800

    Add Passport Register API

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/login', require('./login'));
+router.use('/register', require('./register'));
 router.use('/logout', require('./logout'));
 router.use('/user', require('./user'));
 
---
+++ routes/api/register.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router({mergeParams: true});
+var passport = require('passport');
+var usersData = require('../../models/users-data-schema');
+
+router.post('/', function(req, res, next) {
+  var userData = {
+    username: req.body.username,
+    name: req.body.name,
+    email: req.body.email
+  };
+
+  usersData.register(new usersData(userData), req.body.password, function(err) {
+    if (!err) {
+      passport.authenticate('local', function(err, user) {
+        req.logIn(user, function(err) {
+          if (!err ) {
+            res.status(200).send({
+              success: true, 
+              message: 'Login Successful.'
+            });
+          }
+          else {
+             res.status(500).send({
+              success: false, 
+              message: 'Server Error!'
+            });
+          }
+        })
+      })(req, res, next);
+    }
+    else {
+      res.status(401).send({
+        success: false, 
+        message: 'Username already exist.'
+      });
+    }
+  });
+});
+
+module.exports = router;
