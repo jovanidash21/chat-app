a5fef4e6446023acb0720367b418a39751561c5e
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-09-15 23:00:59 +0800

    Update Redux Store

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,17 @@ import { createStore, applyMiddleware} from 'redux';
 import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
+import { loadingBarMiddleware } from 'react-redux-loading-bar';
 import reducers from '../reducers/index';
 
 const store = createStore(
   reducers,
-  applyMiddleware(createLogger(), thunk, promise())
+  applyMiddleware(
+    thunk,
+    promise(),
+    loadingBarMiddleware(),
+    createLogger()
+  )
 );
 
 export default store;
