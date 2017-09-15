7a513610fbe08a140c802612c2a7d9ca4ded914e
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-09-15 23:00:27 +0800

    Update Local Login Component

--- src/components/Login/LocalLogin.js
+++ src/components/Login/LocalLogin.js
@@ -5,23 +5,64 @@ import Button from 'muicss/lib/react/button';
 require('../../styles/Login.scss');
 
 class LocalLogin extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.onUsernameChange = this.onUsernameChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+    this.handleLocalLogin = this.handleLocalLogin.bind(this);
+  }
+  onUsernameChange(event) {
+    event.preventDefault();
+
+    this.setState({username: event.target.value});
+  }
+  onPasswordChange(event) {
+    event.preventDefault();
+
+    this.setState({password: event.target.value});
+  }
+  handleLocalLogin(event) {
+    event.preventDefault();
+
+    const { 
+      handleLocalLogin
+    } = this.props;
+    const { 
+      username,
+      password
+    } = this.state;
+    let data = {username, password};
+
+    handleLocalLogin(data);
+  }
   render() {
+    const {
+      onUsernameChange,
+      onPasswordChange,
+      handleLocalLogin
+    } = this;
+
     return (
-      <Form>
+      <Form onSubmit={handleLocalLogin}>
         <Input 
           label="Username"
           name="username"
           type="text"
           floatingLabel={true}
           required={true}
-          ref="username"
+          onChange={onUsernameChange}
         />  
         <Input 
           label="Password"
           name="password"
           type="password"
           floatingLabel={true}
-          ref="password"
+          onChange={onPasswordChange}
         />
         <Button
           className="button button-login"
