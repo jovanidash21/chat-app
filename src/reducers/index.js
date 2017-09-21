14b4f9cec3634dd14405cb37b476303472c41a47
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-09-21 14:49:41 +0800

    Update Reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import { loadingBarReducer } from 'react-redux-loading-bar';
 import localLogin from './login/localLogin';
+import register from './register';
 
 const reducers = combineReducers({
   router: routerReducer,
   loadingBar: loadingBarReducer,
-  localLogin
+  localLogin,
+  register
 });
 
 export default reducers;
