18c885b4bd1a8509aa870542beb50090979775b0
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-09-23 17:47:54 +0800

    Update Server Routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Chat App' });
+router.get(['/', '/register'], function(req, res, next) {
+  if (!req.user) { 
+    res.render('index', { title: 'Chat App' });
+  } else {
+    res.redirect('/chat');
+  }
+});
+
+router.get('/chat', function(req, res, next) {
+  if (req.user) { 
+    res.render('index', { title: 'Chat App' });
+  } else {
+    res.redirect('/');
+  }
 });
 
 module.exports = router;
