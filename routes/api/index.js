bbc108aaad7f99107ed07b4043c8443ac5fc11a3
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-10-08 21:15:45 +0800

    Add Nodemailer Send Email API

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 router.use('/login', require('./login'));
 router.use('/register', require('./register'));
 router.use('/logout', require('./logout'));
+router.use('/send-email', require('./send-email'));
 router.use('/user', require('./user'));
 
 module.exports = router;
---
+++ routes/api/send-email.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router({mergeParams: true});
+var nodemailer = require('nodemailer');
+
+router.post('/', function(req, res, next){
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: 'chatapp921@gmail.com',
+      clientId: process.env.GMAIL_CLIENT_ID,
+      clientSecret: process.env.GMAIL_CLIENT_SECRET,
+      refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+      accessToken: process.env.GMAIL_ACCESS_TOKEN,
+    }
+  });
+  var mailOptions = {
+    from: 'Chat App <noreply@chatapp.com>',
+    to: req.body.email,
+    subject: 'Welcome to Chat App',
+    text: 'Hi ' + req.body.username + ', Thank you for creating an account.',
+    html: '<p>Hi ' + req.body.username + ', Thank you for creating an account.</p>'
+  };
+  transporter.sendMail(mailOptions, function(err, response) {
+    if (!err) { 
+      res.status(200).send({
+        success: true, 
+        message: 'Email Sent.'
+      }); 
+    } else {
+      res.status(500).send({
+        success: false, 
+        message: 'Server Error!'
+      });  
+    }
+    transporter.close();
+  });
+});
+
+module.exports = router;
