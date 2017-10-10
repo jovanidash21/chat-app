d54481f37852f5dbbed638f63baf4fb8f7275357
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-10-10 18:52:04 +0800

    Add Reset Redux Store State On Logout

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,21 @@ import { routerReducer } from 'react-router-redux';
 import { loadingBarReducer } from 'react-redux-loading-bar';
 import auth from './auth';
 import email from './email';
+import { LOGOUT } from '../constants/auth';
 
-const reducers = combineReducers({
+const appReducer = combineReducers({
   router: routerReducer,
   loadingBar: loadingBarReducer,
   auth,
   email
 });
 
-export default reducers;
+const rootReducer = (state, action) => {
+  if (action.type === `${LOGOUT}_SUCCESS`) {
+    state = undefined;
+  }
+
+  return appReducer(state, action);
+}
+
+export default rootReducer;
