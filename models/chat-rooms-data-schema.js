59513e122bca3b1c036b720b0deb307237b0cb4b
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-10-18 18:48:20 +0800

    Add Chat Rooms Data Schema

---
+++ models/chat-rooms-data-schema.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var Promise = require('bluebird');
+var Schema = mongoose.Schema;
+var timestamps = require('mongoose-timestamp');
+
+mongoose.Promise = Promise;
+
+var chatRoomsDataSchema = new Schema
+(
+  {
+    text: String,
+    private: Boolean,
+    members: [{
+      type: Schema.Types.ObjectId,
+      ref: 'usersData'
+    }]
+  },
+  {
+    collection: 'chatRoomsData'
+  }
+);
+
+chatRoomsDataSchema.plugin(timestamps);
+
+module.exports = mongoose.model('chatRoomsData', chatRoomsDataSchema);
