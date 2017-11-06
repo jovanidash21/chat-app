7ed9c4bd6ea0d6620a86ed0f88d212b6c86daf5b
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-11-06 20:27:34 +0800

    Update Redux Containers

--- src/containers/index.js
+++
@@ -1,8 +0,0 @@
-// Login
-export { default as Login } from './Login';
-
-// Register
-export { default as Register } from './Register';
-
-// Chat
-export { default as Chat } from './Chat';
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import LoadingBar from 'react-redux-loading-bar';
-import {
-  Login,
-  Register,
-  Chat
-} from '../containers';
+import Login from '../containers/Login';
+import Register from '../containers/Register';
+import Chat from '../containers/Chat';
 
 const routes = (
   <div>
