ce7ab45301eedf8edc94819818da8ad3d560d1a1
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-12-17 22:10:19 +0800

    Update Side Drawer Chat Room Component

--- src/components/SideDrawer/ChatRoom.js
+++ src/components/SideDrawer/ChatRoom.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const ChatRoom = (props) => {
-  return (
-    <div className="chat-room">
-      <div className="chat-room-icon" style={{backgroundImage: `url(${props.chatRoomData.chatIcon})`}}></div>
-      <div className="chat-room-name">
-        {props.chatRoomData.name}
+class ChatRoom extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const { chatRoomData } = this.props;
+
+    return (
+      <div className="chat-room">
+        <div className="chat-room-icon" style={{backgroundImage: `url(${chatRoomData.chatIcon})`}}></div>
+        <div className="chat-room-name">
+          {chatRoomData.name}
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 ChatRoom.propTypes = {
