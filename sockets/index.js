81e256d5cf5430a4a1ffd9dab652a920bd0d30ee
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-12-24 00:01:07 +0800

    Delete Socket.IO Events

--- sockets/index.js
+++ sockets/index.js
@@ -1,31 +1,5 @@
 var sockets = function(socket) {
-  socket.on('user logged in', function (user) {
-    socket.broadcast.emit('user logged in broadcast', user);
-  });
 
-  socket.on('typing', function (typer, chatRoom) {
-    socket.broadcast.to(chatRoom).emit('typing broadcast', typer);
-  });
-
-  socket.on('not typing', function (typer, chatRoom) {
-    socket.broadcast.to(chatRoom).emit('not typing broadcast', typer);
-  });
-
-  socket.on('join chat room', function(chatRoom) {
-    socket.join(chatRoom)
-  })
-
-  socket.on('leave chat room', function(chatRoom) {
-    socket.leave(chatRoom)
-  });
-
-  socket.on('new chat room', function(chatRoom) {
-    socket.broadcast.emit('new chat room broadcast', chatRoom)
-  });
-
-  socket.on('new message', function(message, chatRoom) {
-    socket.broadcast.to(chatRoom).emit('new message broadcast', message);
-  });
 };
 
 module.exports = sockets;
