a9c1c1c9e719d97c04a0432374a874ce2092a021
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-12-25 20:55:14 +0800

    Add Get All Users API

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,14 +1,35 @@
 var express = require('express');
 var router = express.Router({mergeParams: true});
+var usersData = require('../../models/users-data-schema');
 
 router.get('/', function(req, res, next) {
   if (req.user === undefined) {
     res.status(401).send({
-      success: false, 
+      success: false,
       message: 'Unauthorized'
     });
   } else {
     res.status(200).json(req.user);
+  }
+});
+
+router.get('/all', function(req, res, next) {
+  if (req.user === undefined) {
+    res.status(401).send({
+      success: false,
+      message: 'Unauthorized'
+    });
+  } else {
+    usersData.find({}, function(err, users) {
+      if (!err) {
+        res.status(200).send(users);
+      } else {
+        res.status(500).send({
+          success: false,
+          message: 'Server Error!'
+        });
+      }
+    });
   }
 });
 
