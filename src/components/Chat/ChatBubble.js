07b818e57f4e4e0f1615aa3f86d70c7efa463bad
Jovani Cadorna Warguez <jwardash21@gmail.com>
2018-02-06 21:25:36 +0800

    Update Chat Bubble Component

--- src/components/Chat/ChatBubble.js
+++ src/components/Chat/ChatBubble.js
@@ -1,53 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Emojione } from 'react-emoji-render';
 import TimeAgo from 'react-timeago';
 import moment from 'moment';
 import './styles.scss';
 
-class ChatBubble extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const {
-      userData,
-      message,
-      time,
-      isSender
-    } = this.props;
-
-    return (
-      <div className={"chat-bubble-wrapper " + (isSender ? 'reverse' : '')} >
-        <div
-          className={"chat-image " + (!isSender ? 'left' : '')}
-          style={{backgroundImage: `url(${userData.profilePicture})`}}
-          title={userData.name}
-        />
-        <div className="chat-message">
-          {
-            !isSender &&
-            <div className="chat-user-name">{userData.name}</div>
-          }
-          <div className={"chat-bubble " + (isSender ? 'right' : '')}>
-            <div className="chat-text">
-              <Emojione text={message} />
-            </div>
+const ChatBubble = (props) => {
+  return (
+    <div className={"chat-bubble-wrapper " + (props.isSender ? 'reverse' : '')} >
+      <div
+        className={"chat-image " + (!props.isSender ? 'left' : '')}
+        style={{backgroundImage: `url(${props.userData.profilePicture})`}}
+        title={props.userData.name}
+      />
+      <div className="chat-message">
+        {
+          !props.isSender &&
+          <div className="chat-user-name">{props.userData.name}</div>
+        }
+        <div className={"chat-bubble " + (props.isSender ? 'right' : '')}>
+          <div className="chat-text">
+            <Emojione text={props.message} />
           </div>
         </div>
-        {
-          time &&
-          <div className="chat-time">
-            <TimeAgo
-              date={moment(time).format("MMM D, YYYY h:mm:ss A")}
-              title={moment(time).format("dddd - MMM D, YYYY - h:mm A")}
-              minPeriod={60}
-            />
-          </div>
-        }
       </div>
-    )
-  }
+      {
+        props.time &&
+        <div className="chat-time">
+          <TimeAgo
+            date={moment(props.time).format("MMM D, YYYY h:mm:ss A")}
+            title={moment(props.time).format("dddd - MMM D, YYYY - h:mm A")}
+            minPeriod={60}
+          />
+        </div>
+      }
+    </div>
+  )
 }
 
 ChatBubble.propTypes = {
