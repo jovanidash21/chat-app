827519eb87651b287f5fe0e6d9c2309024d656d3
Jovani Cadorna Warguez <jwardash21@gmail.com>
2018-05-17 17:28:29 +0800

    Update Chat Bubble Component

--- src/components/Chat/ChatBubble.js
+++ src/components/Chat/ChatBubble.js
@@ -1,41 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Emojione } from 'react-emoji-render';
+import emojione from 'emojione';
+import ReactHtmlParser from 'react-html-parser';
 import TimeAgo from 'react-timeago';
 import moment from 'moment';
 import './styles.scss';
 
-const ChatBubble = (props) => {
-  return (
-    <div className={"chat-bubble-wrapper " + (props.isSender ? 'reverse' : '')} >
-      <div
-        className={"chat-image " + (!props.isSender ? 'left' : '')}
-        style={{backgroundImage: `url(${props.userData.profilePicture})`}}
-        title={props.userData.name}
-      />
-      <div className="chat-message">
-        {
-          !props.isSender &&
-          <div className="chat-user-name">{props.userData.name}</div>
-        }
-        <div className={"chat-bubble " + (props.isSender ? 'right' : '')}>
-          <div className="chat-text">
-            <Emojione text={props.message} onlyEmojiClassName="emoji-large" />
-          </div>
-        </div>
-      </div>
-      {
-        props.time &&
-        <div className="chat-time">
-          <TimeAgo
-            date={moment(props.time).format("MMM D, YYYY h:mm:ss A")}
-            title={moment(props.time).format("dddd - MMM D, YYYY - h:mm A")}
-            minPeriod={60}
-          />
-        </div>
+class ChatBubble extends Component {
+  constructor(props) {
+    super(props);
+  }
+  handleConvertEmojiTextToImage() {
+    const { message } = this.props;
+    var messageText = message;
+
+    messageText = messageText.replace(/:[^>]*:/g, (match, i, original) => {
+      return ',' + match + ',';
+    });
+
+    var messageArray = messageText.split(",");
+
+    for ( var i = 0; i < messageArray.length; i++ ) {
+      if ( /:[^>]*:/.test(messageArray[i]) ) {
+        messageArray[i] = ReactHtmlParser(emojione.toImage(messageArray[i]));
       }
-    </div>
-  )
+    }
+
+    return messageArray;
+  }
+ render() {
+    const {
+      userData,
+      time,
+      isSender
+    } = this.props;
+
+    return (
+      <div className={"chat-bubble-wrapper " + (isSender ? 'reverse' : '')} >
+        <div
+        className={"chat-image " + (!isSender ? 'left' : '')}
+        style={{backgroundImage: `url(${userData.profilePicture})`}}
+        title={userData.name}
+        />
+          <div className="chat-message">
+            {
+              !isSender &&
+              <div className="chat-user-name">{userData.name}</div>
+            }
+            <div className={"chat-bubble " + (isSender ? 'right' : '')}>
+              <div className="chat-text">
+                {::this.handleConvertEmojiTextToImage()}
+              </div>
+            </div>
+          </div>
+          {
+            time &&
+            <div className="chat-time">
+              <TimeAgo
+              date={moment(time).format("MMM D, YYYY h:mm:ss A")}
+              title={moment(time).format("dddd - MMM D, YYYY - h:mm A")}
+              minPeriod={60}
+              />
+            </div>
+          }
+      </div>
+    )
+  }
 }
 
 ChatBubble.propTypes = {
