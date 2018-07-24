55ab044254f82f8beb625d35d7fbd0f3e62fa1a0
Jovani Cadorna Warguez <jwardash21@gmail.com>
2018-07-24 21:05:13 +0800

    Update Table Pagination Component

--- src/admin/components/Table/Pagination/Index.js
+++ src/admin/components/Table/Pagination/Index.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'muicss/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './styles.scss';
 
 class Pagination extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      numberOfPages: 0
+    };
+  }
+  componentWillMount() {
+    const {
+      totalCount,
+      itemsCountPerPage
+    } = this.props;
+    const numberOfPages = Math.ceil(totalCount / itemsCountPerPage);
+
+    this.setState({numberOfPages: numberOfPages});
+
   }
   handleChangePage(event, page) {
     event.preventDefault();
@@ -20,20 +35,45 @@ class Pagination extends Component {
       totalCount,
       itemsCountPerPage
     } = this.props;
+    const { numberOfPages } = this.state;
     const pageNumbers = [];
+    const pageNumbersWithDots = [];
+    var delta = 1;
+    var left = activePage - delta;
+    var right = activePage + delta + 1;
+    var j;
 
-    for ( var i = 1; i <= Math.ceil(totalCount / itemsCountPerPage); i++ ) {
-      pageNumbers.push(i);
+    for ( var i = 1; i <= numberOfPages; i++ ) {
+      if (i == 1 || i == numberOfPages || i >= left && i < right) {
+        pageNumbers.push(i);
+      }
+    }
+
+    for ( var i of pageNumbers ) {
+      if (j) {
+        if (i - j === 2) {
+          pageNumbersWithDots.push(j + 1);
+        } else if (i - j !== 1) {
+          pageNumbersWithDots.push('...');
+        }
+      }
+      pageNumbersWithDots.push(i);
+      j = i;
     }
 
     return (
-      pageNumbers.map((number, i) =>
+      pageNumbersWithDots.map((number, i) =>
         <li key={i}>
           <Button
-            className={"pagination-button " + (number === activePage ? 'active' : '')}
+            className={
+              "pagination-button " +
+              (number === activePage ? 'active ' : '') +
+              (number === '...' ? 'ellipsis-button' : '')
+            }
             variant="flat"
             size="small"
-            onClick={(e) => ::this.handleChangePage(e, number)}
+            onClick={number !== '...' ? (e) => ::this.handleChangePage(e, number) : false}
+            disabled={number === '...'}
           >
             {number}
           </Button>
@@ -47,32 +87,55 @@ class Pagination extends Component {
       totalCount,
       itemsCountPerPage
     } = this.props;
+    const { numberOfPages } = this.state;
     const isPrevButtonDisabled = activePage === 1;
-    const isNextButtonDisabled = activePage === Math.ceil(totalCount / itemsCountPerPage);
+    const isNextButtonDisabled = activePage === numberOfPages;
 
     return (
       <ul className="pagination-buttons">
         <li>
           <Button
-            className="pagination-button"
+            className="pagination-button arrow-button"
+            variant="flat"
+            size="small"
+            onClick={!isPrevButtonDisabled ? (e) => ::this.handleChangePage(e, 1) : false}
+            disabled={isPrevButtonDisabled}
+          >
+            <FontAwesomeIcon icon="angle-double-left" />
+          </Button>
+        </li>
+        <li>
+          <Button
+            className="pagination-button arrow-button"
             variant="flat"
             size="small"
             onClick={!isPrevButtonDisabled ? (e) => ::this.handleChangePage(e, activePage - 1) : false}
             disabled={isPrevButtonDisabled}
           >
-            Prev
+            <FontAwesomeIcon icon="angle-left" />
           </Button>
         </li>
         {::this.handlePaginationRender()}
         <li>
           <Button
-            className="pagination-button"
+            className="pagination-button arrow-button"
             variant="flat"
             size="small"
             onClick={!isNextButtonDisabled ? (e) => ::this.handleChangePage(e, activePage + 1) : false}
             disabled={isNextButtonDisabled}
           >
-            Next
+            <FontAwesomeIcon icon="angle-right" />
+          </Button>
+        </li>
+        <li>
+          <Button
+            className="pagination-button arrow-button"
+            variant="flat"
+            size="small"
+            onClick={!isNextButtonDisabled ? (e) => ::this.handleChangePage(e, numberOfPages) : false}
+            disabled={isNextButtonDisabled}
+          >
+            <FontAwesomeIcon icon="angle-double-right" />
           </Button>
         </li>
       </ul>
