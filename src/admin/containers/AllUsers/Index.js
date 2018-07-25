b47dec562231f1b6abf460b1e2c46e94ad338133
Jovani Cadorna Warguez <jwardash21@gmail.com>
2018-07-25 21:15:43 +0800

    Update All Users Container

--- src/admin/containers/AllUsers/Index.js
+++ src/admin/containers/AllUsers/Index.js
@@ -4,32 +4,66 @@ import { Panel } from 'muicss/react';
 import mapDispatchToProps from '../../actions';
 import Table from '../Partial/Table';
 
-const columns = [
-  { key: 'name', label: 'Name' },
-  { key: 'email', label: 'Email' },
-  { key: 'accountType', label: 'Account Type' },
-  { key: 'role', label: 'Role' },
-];
-
 class AllUsers extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      isLoading: true,
+      columns: [
+        { key: 'name', label: 'Name' },
+        { key: 'email', label: 'Email' },
+        { key: 'accountType', label: 'Account Type' },
+        { key: 'role', label: 'Role' }
+      ],
+      rows: []
+    };
   }
   componentWillMount() {
     const { fetchUsers } = this.props;
 
     fetchUsers();
   }
-  render() {
+  componentDidUpdate(prevProps) {
+    if ( prevProps.user.isFetchingAll && !this.props.user.isFetchingAll ) {
+      ::this.handleUserRows();
+    }
+  }
+  handleUserRows() {
     const { user } = this.props;
+    const userRows = [];
+
+    for ( var i = 0; i < user.all.length; i++ ) {
+      const singleUser = user.all[i];
+
+      userRows.push({
+        image: singleUser.profilePicture,
+        name: singleUser.name,
+        email: singleUser.email,
+        accountType: singleUser.accountType,
+        role: singleUser.role
+      });
+    }
+
+    this.setState({
+      isLoading: false,
+      rows: userRows
+    });
+  }
+  render() {
+    const {
+      isLoading,
+      columns,
+      rows
+    } = this.state;
 
     return (
       <div className="all-users-section">
         <Panel>
           <Table
             columns={columns}
-            rows={user.all}
-            isLoading={user.isFetchingAll}
+            rows={rows}
+            isLoading={isLoading}
           />
         </Panel>
       </div>
