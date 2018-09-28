da1d9c57ffe0849e59b1830dcf565d7264093709
Jovani Cadorna Warguez <jwardash21@gmail.com>
2018-09-28 16:29:41 +0800

    Update Form Input Component

--- src/_main/components/Form/index.js
+++ src/_main/components/Form/index.js
@@ -1,4 +1,4 @@
+export { default as Input } from './Input';
 export { default as LoginButton } from './Button/LoginButton';
 export { default as RegisterButton } from './Button/RegisterButton';
 export { default as SocialButton } from './Button/SocialButton';
-export { Input } from './Input';
