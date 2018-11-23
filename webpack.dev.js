6192a48c153ee12c6a1d75a60eab78c8ec5941d3
Jovani Cadorna Warguez <jwardash21@gmail.com>
2018-11-23 22:30:24 +0800

    Upgrade Webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,12 +5,6 @@ const webpack = require('webpack');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+  mode: 'development',
   devtool: 'source-map',
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('development'),
-      },
-    }),
-  ],
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,26 +5,5 @@ const webpack = require('webpack');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      mangle: false,
-      compress: {
-        warnings: false,
-        pure_getters: true,
-        unsafe: true,
-        unsafe_comps: true,
-        screw_ie8: true,
-      },
-      output: {
-        comments: false,
-      },
-      exclude: [/\.min\.js$/gi],
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production'),
-      },
-    }),
-    new webpack.NoEmitOnErrorsPlugin(),
-  ],
+  mode: 'production',
 });
