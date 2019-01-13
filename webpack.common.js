2cdf849b4227a689a14102529922ed9b5cbb456d
Jovani Cadorna Warguez <jwardash21@gmail.com>
2019-01-13 14:09:12 +0800

    Update Webpack Config

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,6 @@
 const path = require('path');
 const autoprefixer = require('autoprefixer');
 const postcssFlexbugsFixes = require('postcss-flexbugs-fixes');
-const adapter = require.resolve('webrtc-adapter');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const extractSassBundle = new ExtractTextPlugin({
@@ -97,10 +96,6 @@ module.exports = {
           ],
           fallback: 'style-loader',
         }),
-      },
-      {
-        test: adapter,
-        loader: 'expose-loader',
       },
     ],
   },
