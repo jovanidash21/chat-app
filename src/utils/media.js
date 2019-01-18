e0f20e0ec653aba37fb9fa43d6a12b23724be30b
Jovani Cadorna Warguez <jwardash21@gmail.com>
2019-01-18 23:37:45 +0800

    Add Get Media Utils Function

---
+++ src/utils/media.js
@@ -0,0 +1,30 @@
+/**
+ * Get media
+ * @param {function} callback
+ * @param {function} err
+ */
+export function getMedia(callback, err) {
+  const constraints = {
+    video: {
+      facingMode: 'user',
+      height: {
+        min: 360,
+        ideal: 720,
+        max: 1080
+      }
+    },
+    audio: true
+  };
+
+  if ( navigator.mediaDevices && navigator.mediaDevices.getUserMedia ) {
+    return navigator.mediaDevices.getUserMedia(constraints)
+      .then((stream) => {
+        callback(stream);
+      })
+      .catch((error) => {
+        err(error);
+      });
+  }
+
+  return navigator.getUserMedia(constraints, callback,  err);
+}
