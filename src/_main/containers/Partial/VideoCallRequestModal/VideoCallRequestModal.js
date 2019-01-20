38e3e5fade60aca073c3bc28ab700b3b0b2f0f03
Jovani Cadorna Warguez <jwardash21@gmail.com>
2019-01-20 20:10:07 +0800

    Add Video Call Request Modal And Video Call Window Container

---
+++ src/_main/containers/Partial/VideoCallRequestModal/VideoCallRequestModal.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import mapDispatchToProps from '../../../actions';
+import { Modal } from '../../../../components/Modal';
+import { Avatar } from '../../../../components/Avatar';
+import './styles.scss';
+
+class VideoCallRequestModal extends Component {
+  constructor(props) {
+    super(props);
+  }
+  handleAcceptVideoCall(event) {
+    event.preventDefault();
+
+    const {
+      videoCall,
+      handleAcceptVideoCall
+    } = this.props;
+    const peerUser = videoCall.peerUser;
+
+    handleAcceptVideoCall(peerUser._id);
+  }
+  handleRejectVideoCall(event) {
+    event.preventDefault();
+
+    const { handleRejectVideoCall } = this.props;
+
+    handleRejectVideoCall();
+  }
+  render() {
+    const {
+      isModalOpen,
+      videoCall
+    } = this.props;
+    const peerUser = videoCall.peerUser;
+
+    return (
+      <Modal
+        className="video-call-request-modal"
+        open={isModalOpen}
+        showCloseIcon={false}
+      >
+        <Modal.Body>
+          <div className="avatar-wrapper">
+            <Avatar
+              image={peerUser.profilePicture}
+              size="100px"
+              name={peerUser.name}
+              roleChatType={peerUser.role}
+              accountType={peerUser.accountType}
+              badgeBigger
+              badgeCloser
+            />
+          </div>
+          <p>
+            <span className="user-name mui--text-danger">{peerUser.name}</span>&nbsp;
+            is video calling ...
+          </p>
+          <div className="video-call-controls">
+            <div className="video-call-button accept-call-button" title="Accept Video Call" onClick={::this.handleAcceptVideoCall}>
+              <FontAwesomeIcon icon="video" size="2x" />
+            </div>
+            <div className="video-call-button end-call-button" title="Reject Video Call" onClick={::this.handleRejectVideoCall}>
+              <FontAwesomeIcon icon="phone" size="2x" />
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    videoCall: state.videoCall
+  }
+}
+
+VideoCallRequestModal.propTypes = {
+  isModalOpen: PropTypes.bool,
+  handleAcceptVideoCall: PropTypes.func.isRequired,
+  handleRejectVideoCall: PropTypes.func.isRequired
+}
+
+VideoCallRequestModal.defaultProps = {
+  isModalOpen: false
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(VideoCallRequestModal);
--- src/_main/containers/Partial/index.js
+++ src/_main/containers/Partial/index.js
@@ -6,3 +6,5 @@ export { CreateChatRoomModal } from './CreateChatRoomModal';
 export { DeleteMessageModal } from './DeleteMessageModal';
 export { MembersList } from './MembersList';
 export { MuteUnmuteChatRoomModal } from './MuteUnmuteChatRoomModal';
+export { VideoCallRequestModal } from './VideoCallRequestModal';
+export { VideoCallWindow } from './VideoCallWindow';
