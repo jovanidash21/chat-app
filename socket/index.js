415f356815026fee0a6b723fd4cbe15958b87667
Jovani Cadorna Warguez <jwardash21@gmail.com>
2019-03-24 18:47:50 +0800

    Update Send Message Socket.IO Event

--- socket/index.js
+++ socket/index.js
@@ -100,23 +100,26 @@ var sockets = function(io) {
                         message: action.message
                       });
                     } else {
-                      if (chatRoom.chatType === 'direct') {
-                        chatRoom.name = action.message.user.name;
-                      }
+                      var chatRoomIndex = user.chatRooms.findIndex(singleChatRoom => {
+                        return singleChatRoom.data._id == action.chatRoomID && !singleChatRoom.mute.data;
+                      });
 
-                      for (var j = 0; j < user.chatRooms.length; j++) {
-                        var singleChatRoom = user.chatRooms[j];
+                      if (chatRoomIndex > -1) {
+                        var singleChatRoom = user.chatRooms[chatRoomIndex];
 
-                        if ( singleChatRoom.data._id == action.chatRoomID && !singleChatRoom.mute.data ) {
-                          socket.broadcast.to(user.socketID).emit('action', {
-                            type: 'SOCKET_BROADCAST_NOTIFY_MESSAGE',
-                            chatRoom: singleChatRoom,
-                            chatRoomID: action.chatRoomID,
-                            chatRoomName: chatRoom.name,
-                            senderName: action.message.user.name
-                          });
-                          break;
+                        if (singleChatRoom.data.chatType === 'direct') {
+                          singleChatRoom.data.name = action.message.user.name;
+                          singleChatRoom.data.chatIcon = action.message.user.profilePicture;
+                          singleChatRoom.data.members = chatRoom.members;
                         }
+
+                        socket.broadcast.to(user.socketID).emit('action', {
+                          type: 'SOCKET_BROADCAST_NOTIFY_MESSAGE',
+                          chatRoom: singleChatRoom,
+                          chatRoomID: action.chatRoomID,
+                          chatRoomName: singleChatRoom.data.name,
+                          senderName: action.message.user.name
+                        });
                       }
                     }
                   })
