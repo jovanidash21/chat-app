b4866a97a4d26f16ee66d9e52cc3e72e8b4b5b42
Jovani Cadorna Warguez <jwardash21@gmail.com>
2019-03-26 23:00:56 +0800

    Update Webpack Config

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,8 +3,12 @@
 const merge = require('webpack-merge');
 const webpack = require('webpack');
 const common = require('./webpack.common.js');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'source-map',
+  plugins: [
+    new FriendlyErrorsWebpackPlugin(),
+  ],
 });
