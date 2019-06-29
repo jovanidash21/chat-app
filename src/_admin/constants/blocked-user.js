e2d363d89e39500638c1f02760d07709b06e3856
Jovani Cadorna Warguez <jwardash21@gmail.com>
2019-06-29 17:38:01 +0800

    Add Admin Blocked Users Constant

---
+++ src/_admin/constants/blocked-user.js
@@ -0,0 +1,4 @@
+export const FETCH_BLOCKED_USERS = 'FETCH_BLOCKED_USERS';
+export const BLOCK_USER = 'BLOCK_USER';
+export const UNBLOCK_USER = 'UNBLOCK_USER';
+export const UNBLOCK_ALL_USERS = 'UNBLOCK_ALL_USERS';
