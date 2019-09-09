a3b85b0b72c67cf3e74c0d677bd843796d5d8657
Jovani Cadorna Warguez <jwardash21@gmail.com>
2019-09-09 22:09:11 +0800

    Update Date Utils Functions

--- src/utils/date.js
+++ src/utils/date.js
@@ -3,11 +3,11 @@
  *
  * @param {string} date
  */
-export function isDateToday( date ) {
-  const d = new Date( date );
+export function isDateToday(date) {
+  const d = new Date(date);
   const todayDate = new Date();
 
-  if ( d.setHours( 0, 0, 0, 0 ) === todayDate.setHours( 0, 0, 0, 0 ) ) {
+  if (d.setHours( 0, 0, 0, 0 ) === todayDate.setHours(0, 0, 0, 0)) {
     return true;
   }
 
@@ -19,13 +19,13 @@ export function isDateToday( date ) {
  *
  * @param {string} date
  */
-export function isDateYesterday( date ) {
+export function isDateYesterday(date) {
   const d = new Date( date );
   const todayDate = new Date();
   const yesterdayDate = new Date();
-  yesterdayDate.setDate( yesterdayDate.getDate() - 1 );
+  yesterdayDate.setDate(yesterdayDate.getDate() - 1);
 
-  if ( d.setHours( 0, 0, 0, 0 ) === yesterdayDate.setHours( 0, 0, 0, 0 ) ) {
+  if (d.setHours(0, 0, 0, 0) === yesterdayDate.setHours(0, 0, 0, 0)) {
     return true;
   }
 
@@ -37,11 +37,11 @@ export function isDateYesterday( date ) {
  *
  * @param {string} date
  */
-export function isDateThisYear( date ) {
-  const d = new Date( date );
+export function isDateThisYear(date) {
+  const d = new Date(date);
   const todayDate = new Date();
 
-  if ( d.getFullYear() === todayDate.getFullYear() ) {
+  if (d.getFullYear() === todayDate.getFullYear()) {
     return true;
   }
 
@@ -54,16 +54,16 @@ export function isDateThisYear( date ) {
  * @param {string} date1
  * @param {string} date2
  */
-export function isDatesSameDay( date1, date2 ) {
-  const d1 = new Date( date1 );
-  const d2 = new Date( date2 );
+export function isDatesSameDay(date1, date2) {
+  const d1 = new Date(date1);
+  const d2 = new Date(date2);
 
   if (
     d1.getFullYear() === d2.getFullYear() &&
     d1.getMonth() === d2.getMonth() &&
     d1.getDate() === d2.getDate()
   ) {
-    return true
+    return true;
   }
 
   return false;
